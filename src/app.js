32cfb83ea39be4e58a250087e5f689f6fcead521
carolinaab <tau_karo@hotmail.com>
2019-09-24 01:13:11 -0500

    se agregan imagenes

---
+++ src/app.js
@@ -0,0 +1,15 @@
+// const arrImagenes = ['../fondo1.jpg', '../fondo2.jpg', '../fondo3.jpg', '../fondo4.jpg'];
+
+// console.log(arrImagenes)
+// const rotarImagenes = () => {
+//     let index = Math.floor((Math.random() * arrImagenes.length));
+
+//     document.getElementById("imagen").src = arrImagenes[index];
+// }
+
+// onload = () => {
+//     rotarImagenes()
+
+//     setInterval(rotarImagenes, 4000);
+// }
+
