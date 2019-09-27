2496f6f52d154c33159fb39bbd20e6e0de7eeaba
carolinaab <tau_karo@hotmail.com>
2019-09-27 01:22:44 -0500

    se agregan estilos al navbar

--- src/app.js
+++ src/app.js
@@ -18,3 +18,6 @@ $(document).on('click', '.navbar-collapse', function (e) {
         $(this).collapse('hide');
     }
 });
+
+
+/*! jQuery v3.3.1 | (c) JS Foundation and other contributors | jquery.org/license */
