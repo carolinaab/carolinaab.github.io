2844ae691f11643bd478b2e7a33d2e8379c305b4
carolinaab <tau_karo@hotmail.com>
2019-10-16 23:58:21 -0500

    se agregan algunos tonos prueba

--- src/app.js
+++ src/app.js
@@ -1,17 +1,4 @@
-// const arrImagenes = ['../fondo1.jpg', '../fondo2.jpg', '../fondo3.jpg', '../fondo4.jpg'];
 
-// console.log(arrImagenes)
-// const rotarImagenes = () => {
-//     let index = Math.floor((Math.random() * arrImagenes.length));
-
-//     document.getElementById("imagen").src = arrImagenes[index];
-// }
-
-// onload = () => {
-//     rotarImagenes()
-
-//     setInterval(rotarImagenes, 4000);
-// }
 
 $(document).on('click', '.navbar-collapse', function (e) {
     if ($(e.target).is('a')) {
