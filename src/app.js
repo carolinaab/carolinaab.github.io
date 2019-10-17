6a8b9ea7b947fc3c0270ee2df3725178b0afbc6b
carolinaab <tau_karo@hotmail.com>
2019-10-17 01:39:36 -0500

    funcion cambio de imagenes

--- src/app.js
+++ src/app.js
@@ -1,4 +1,17 @@
+// cambio de imagenes 
 
+// let arrayImagenes = ["../img/fondo3.jpg", "../img/cipher.png"];
+
+// const cambiarImagenes = () => {
+//     let index = Math.floor((Math.random() * arrayImagenes.length));
+
+//     document.getElementById("imagen").src = arrayImagenes[index];
+// }
+
+// onload = () => {
+//     cambiarImagenes();
+//     setInterval(cambiarImagenes, 5000);
+// }
 
 $(document).on('click', '.navbar-collapse', function (e) {
     if ($(e.target).is('a')) {
