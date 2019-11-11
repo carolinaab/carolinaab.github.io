feb52e76cf33f2193d7076ae9b2f36aecf9394f8
carolinaab <tau_karo@hotmail.com>
2019-11-11 15:40:09 -0600

    secambia liga CV

--- src/app.js
+++ src/app.js
@@ -1,20 +1,3 @@
-// cambio de imagenes 
-
-// let arrayImagenes = ["../img/fondo1.jpg",
-//     "../img/fondo2.jpg",
-//     "../img/fondo4.jpg",
-//     "../img/fondo5.jpg"];
-
-// const cambiarImagenes = () => {
-//     let index = Math.floor((Math.random() * arrayImagenes.length));
-
-//     document.getElementById("imagen").src = arrayImagenes[index];
-// }
-
-// onload = () => {
-//     cambiarImagenes();
-//     setInterval(cambiarImagenes, 4000);
-// }
 
 
 $(document).on('click', '.navbar-collapse', function (e) {
